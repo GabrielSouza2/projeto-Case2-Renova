e1e297be83017107f5ed6a4e7dda1c6ab6e50611
JulianePrado <112557540+JulianePrado@users.noreply.github.com>
2023-03-03 20:08:49 -0300

    Atualização de pastas

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,7 @@ import './assets/css/main.css'
 
 import Menu from './components/Menu'
 import Rodape from './components/Rodape'
-import Rotas from './routes/Rotas.jsx'
+import Rotas from '../routes/Rotas.jsx'
 
 function App() {
     return (
